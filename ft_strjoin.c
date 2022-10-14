4f17ac3b37087219f4d0bba94489a17bbcf2003c
Zixianlow97 <113102979+Zixianlow97@users.noreply.github.com>
2022-10-14 12:35:39 +0800

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lzi-xian <lzi-xian@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 20:29:15 by lzi-xian          #+#    #+#             */
+/*   Updated: 2022/10/12 16:47:38 by lzi-xian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	l;
+	char	*p;
+
+	if (!s1 || !s2)
+		return (NULL);
+	l = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	i = 0;
+	p = (char *) malloc ((l + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		p[i + j] = s2[j];
+		j++;
+	}
+	p[i + j] = '\0';
+	return (p);
+}
